27a9e6de423981b033c8623ff4f62f65ede7d607
Michael Di Giacomi <michaeldigiacomi@icloud.com>
2022-03-24 22:34:53 -0400

    Updated function to Strip results from Lex Bot

---
+++ src/handlers/kinesisHandler.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const zlib = require('zlib');
+var output = {};
+
+/**
+ * A Lambda function that logs the payload received from S3.
+ */
+ exports.kinesisHandler = function(event, context) {
+  if (event.awslogs && event.awslogs.data) {
+    const payload = Buffer.from(event.awslogs.data, 'base64');
+
+    const logevents = JSON.parse(zlib.unzipSync(payload).toString()).logEvents;
+
+    for (const logevent of logevents) {
+      const log = JSON.parse(logevent.message);
+      var s3 = new AWS.S3();
+
+      var params = {
+          Bucket : "gic-lexlogs",
+          Key : log.sessionId + ".json",
+          Body : JSON.stringify(log.interpretations)
+      }
+      s3.putObject(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+      });
+    }
+  }
+};
